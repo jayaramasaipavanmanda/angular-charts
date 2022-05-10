7c1d0f950481cddaf4797756ec38e1457b2ed861
unknown <s.manda@easternenterprise.com>
2022-05-10 20:42:17 +0530

    chore: deleted unwanted header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,11 +7,10 @@ import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from "@angular/platform-browser";
 import { MatButtonModule } from "@angular/material/button";
-import { HeaderComponent } from "./components/header/header.component";
 import { MatIconModule } from "@angular/material";
 
 @NgModule({
-  declarations: [SelectComponent, HeaderComponent],
+  declarations: [SelectComponent],
   imports: [
     CommonModule,
     ChartsModule,
@@ -24,6 +23,6 @@ import { MatIconModule } from "@angular/material";
   ],
   providers: [ThemeService],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  exports: [SelectComponent, MatButtonModule, HeaderComponent],
+  exports: [SelectComponent, MatButtonModule, MatIconModule],
 })
 export class SharedModule {}
